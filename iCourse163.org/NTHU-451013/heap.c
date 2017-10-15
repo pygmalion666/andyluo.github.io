fd58bfe3918ad409f8a64be7e574ebc8bfee878e
[andyluo] <luod5202@163.com>
2017-10-15 11:10:10 +0800

    Modify heap sort

--- iCourse163.org/NTHU-451013/heap.c
+++ iCourse163.org/NTHU-451013/heap.c
@@ -22,23 +22,24 @@ int main()
             currentIndex = i;
             ret[currentIndex - 1] = value;
 
-            while(( parentIndex = (int)(currentIndex / 2) ) > 0){
+            while(( parentIndex = (int)(currentIndex / 2)) > 0){
                 //Change current value to it's parent
-                printf("%d %d\n", currentIndex, parentIndex);
                 if(ret[currentIndex - 1] < ret[parentIndex - 1]){
-                    temp = ret[parentIndex];
-                    ret[parentIndex] = ret[currentIndex - 1];
+                    temp = ret[parentIndex - 1];
+                    ret[parentIndex - 1] = ret[currentIndex - 1];
                     ret[currentIndex - 1] = temp;
 
                     currentIndex = parentIndex;
                 }
-                break;
+		else{
+		    break;
+		}
             }
         }
     }
 
     for(i = 1; i <= 8; i ++){
-        //printf("%d\n", ret[i-1]);
+        printf("%d\n", ret[i-1]);
     }
 
 
